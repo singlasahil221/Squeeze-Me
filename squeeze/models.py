750e58208bc18815054a631751f65593eca9923e
singlasahil221 <singla.sahil221@gmail.com>
2017-12-14 11:46:18 +0530

    Update models.py

--- squeeze/models.py
+++ squeeze/models.py
@@ -1,4 +1,9 @@
 from django.db import models
+from __future__ import unicode_literals
+
+from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+from django.db import models
 
 class Link(models.Model):
     link = models.URLField()
