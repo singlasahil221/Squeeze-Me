a42eb7533e2045d8341e9baf2dfd073918d10fa4
Sahil Singla <singla.sahil221@gmail.com>
2018-01-25 07:25:41 +0530

    Update models.py

--- squeeze/models.py
+++ squeeze/models.py
@@ -8,7 +8,7 @@ from django.db import models
 class Link(models.Model):
     link = models.URLField()
     hits = models.IntegerField(default=0)
-    short_url = models.CharField(max_length=10,primary_key=True)
+    short_url = models.CharField(max_length=50,primary_key=True)
 
     def __str__(self):
         return self.short_url
